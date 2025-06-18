c10c423e347b1b46a7cbfe836fe93d769685f78d
elizavetor <gusevaliza210@yandex.ru>
2025-06-18 10:33:25 +0300

    Реализован слот testIsCurrentOrderOfMultipliers
    Тестовая функция для метода OperandOfExpr::isCurrentOrderOfMultipliers

--- test_iscurrentorderofmultipliers.cpp
+++ test_iscurrentorderofmultipliers.cpp
@@ -1,2 +1,14 @@
 #include "test_iscurrentorderofmultipliers.h"
 
+void Test_isCurrentOrderOfMultipliers::testIsCurrentOrderOfMultipliers()
+{
+    QFETCH(OperandOfExpr, elem_1);
+    QFETCH(OperandOfExpr, elem_2);
+    QFETCH(bool, exp_result);
+
+    // Получить результат сравнения
+    bool real_result = elem_1.isCurrentOrderOfMultipliers(elem_2);
+
+    // Сравнить полученный результат с ожидаемым
+    QCOMPARE(real_result, exp_result);
+}
