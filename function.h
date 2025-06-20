f754ce1f365f4c532edffdeac6a5d563dcb57cf8
elizavetor <gusevaliza210@yandex.ru>
2025-06-20 10:17:01 +0300

    Создание функции postfixToTree
    Объявление функции, реализация псевдокода, установление заглушки.
    ОШИБКА:
    function.h -> nodeofexprtree.h
    nodeofexprtree.h -> function.h

--- function.h
+++ function.h
@@ -1,7 +1,17 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
+#include "error.h"
+#include "nodeofexprtree.h"
 
 #include <QObject>
+
+/*!
+ * \brief Из постфиксной записи выражения получить дерево этого выражения
+ * \param [in] expr － строка выражения в постфиксной записи
+ * \param [out] errors － обнаруженные ошибки в постфиксной записи
+ * \return указатель на корень построенного дерева
+ */
+NodeOfExprTree* postfixToTree(QString expr, QSet<Error> errors);
 
 /*!
  * \brief Определить является ли строка числом
