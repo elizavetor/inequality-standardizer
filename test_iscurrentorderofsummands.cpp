744558cb3d750bc426f03b7ef52ab2f9bb83c24f
elizavetor <gusevaliza210@yandex.ru>
2025-06-25 17:21:48 +0300

    Создание слота Test_isCurrentOrderOfSummands::testIsCurrentOrderOfSummands

--- test_iscurrentorderofsummands.cpp
+++ test_iscurrentorderofsummands.cpp
@@ -3,3 +3,17 @@
 Test_isCurrentOrderOfSummands::Test_isCurrentOrderOfSummands(QObject *parent)
     : QObject{parent}
 {}
+
+void Test_isCurrentOrderOfSummands::testIsCurrentOrderOfSummands()
+{
+    QFETCH(OperandOfExpr, elem_1);
+    QFETCH(OperandOfExpr, elem_2);
+    QFETCH(bool, exp_result);
+
+    // Получить результат сравнения
+    bool real_result = elem_1.isCurrentOrderOfMultipliers(elem_2);
+
+    // Сравнить полученный результат с ожидаемым
+    QCOMPARE(real_result, exp_result);
+}
+
