b5b24aa82ecafd63c3e057a702563deeb4f6a698
elizavetor <gusevaliza210@yandex.ru>
2025-06-25 12:54:39 +0300

    Реализован слот Test_treeToInfix::testTreeToInfix

--- test_treetoinfix.cpp
+++ test_treetoinfix.cpp
@@ -3,3 +3,15 @@
 Test_treeToInfix::Test_treeToInfix(QObject *parent)
     : QObject{parent}
 {}
+
+void Test_treeToInfix::testTreeToInfix()
+{
+    QFETCH(NodeOfExprTree*, tree);
+    QFETCH(QString, exp_infix);
+
+    // Получить инфиксную запись дерева
+    QString real_infix = tree->treeToInfix();
+
+    // Сравнить полученную строку с ожидаемой
+    QCOMPARE(real_infix, exp_infix);
+}
