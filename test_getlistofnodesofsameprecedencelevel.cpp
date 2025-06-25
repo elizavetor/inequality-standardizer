eebe6fe2e284d3de39c09a63f2e2f83cf33a8360
elizavetor <gusevaliza210@yandex.ru>
2025-06-25 08:38:20 +0300

    Реализован слот testGetListOfNodesOfSamePrecedenceLevel

--- test_getlistofnodesofsameprecedencelevel.cpp
+++ test_getlistofnodesofsameprecedencelevel.cpp
@@ -3,3 +3,18 @@
 Test_getListOfNodesOfSamePrecedenceLevel::Test_getListOfNodesOfSamePrecedenceLevel(QObject *parent)
     : QObject{parent}
 {}
+
+void Test_getListOfNodesOfSamePrecedenceLevel::testGetListOfNodesOfSamePrecedenceLevel()
+{
+    QFETCH(NodeOfExprTree*, root);
+    QFETCH(QList<OperandOfExpr>, exp_list);
+
+    // Получить результат
+    QList<OperandOfExpr> real_list = root->getListOfNodesOfSamePrecedenceLevel();
+
+    // Сравнить полученный и ожидаемый списки
+    QString error = compareListsOfNodes(exp_list, real_list);
+    QVERIFY2(error.isEmpty(), qPrintable(error));
+
+}
+
