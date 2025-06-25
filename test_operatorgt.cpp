76ffcaac00cfe64832c52ac37b2b4fec24bba93f
elizavetor <gusevaliza210@yandex.ru>
2025-06-25 15:37:10 +0300

    Создание слота Test_operatorGt::testOperatorGt

--- test_operatorgt.cpp
+++ test_operatorgt.cpp
@@ -3,3 +3,17 @@
 Test_operatorGt::Test_operatorGt(QObject *parent)
     : QObject{parent}
 {}
+
+void Test_operatorGt::testOperatorGt()
+{
+    QFETCH(OperandOfExpr, elem_1);
+    QFETCH(OperandOfExpr, elem_2);
+    QFETCH(bool, exp_result);
+
+    // Получить дерево
+    bool real_result = elem_1 > elem_2;
+
+    // Сравнить полученные ошибки с ожидаемыми
+    QCOMPARE(real_result, exp_result);
+}
+
