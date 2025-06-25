69b1d1aa5da89e92400c3e0962a4cacfd08070f9
elizavetor <gusevaliza210@yandex.ru>
2025-06-25 15:53:46 +0300

    Исправление Test_getSortedList::testGetSortedList_data
    Ошибка в названии столбца
    В последней строке лишние данные

--- test_getsortedlist.cpp
+++ test_getsortedlist.cpp
@@ -21,7 +21,7 @@ void Test_getSortedList::testGetSortedList_data()
 {
     // Добавить столбцы
     QTest::addColumn<NodeOfExprTree*>("root");
-    QTest::addColumn<QList<OperandOfExpr>>("exp_degree");
+    QTest::addColumn<QList<OperandOfExpr>>("exp_list");
 
     // Создать список ошибок для вызова функции создания дерева
     QSet<Error> errors;
@@ -43,39 +43,39 @@ void Test_getSortedList::testGetSortedList_data()
                                    OperandOfExpr{plus, b, false}};
     QTest::newRow("1. Summands without expression in parentheses") << postfixToTree("1 a + 2 b + +", errors) << list_1;
 
-    // // 2. Слагаемые с выражением в скобках, заданный узел есть бинарный минус
-    // QList<OperandOfExpr> list_2 = {OperandOfExpr{nullptr, _1, true},
-    //                                OperandOfExpr{plus, a, false},
-    //                                OperandOfExpr{minus, _2, false},
-    //                                OperandOfExpr{plus, b, false}};
-    // QTest::newRow("2. Terms with an expression in parentheses, a given node is a binary minus") << postfixToTree("1 a + 2 b - -", errors) << list_2;
+    // 2. Слагаемые с выражением в скобках, заданный узел есть бинарный минус
+    QList<OperandOfExpr> list_2 = {OperandOfExpr{nullptr, _1, true},
+                                   OperandOfExpr{plus, a, false},
+                                   OperandOfExpr{minus, _2, false},
+                                   OperandOfExpr{plus, b, false}};
+    QTest::newRow("2. Terms with an expression in parentheses, a given node is a binary minus") << postfixToTree("1 a + 2 b - -", errors) << list_2;
 
-    // // 3. Слагаемые с выражением в скобках, заданный узел есть плюс
-    // QTest::newRow("3. Terms with expressions in parentheses, this node is a plus") << postfixToTree("1 a 2 b - - +", errors) << list_2;
+    // 3. Слагаемые с выражением в скобках, заданный узел есть плюс
+    QTest::newRow("3. Terms with expressions in parentheses, this node is a plus") << postfixToTree("1 a 2 b - - +", errors) << list_2;
 
-    // // 4. Множители без выражений в скобках
-    // QList<OperandOfExpr> list_4 = {OperandOfExpr{nullptr, _1, true},
-    //                                OperandOfExpr{multiply, a, false},
-    //                                OperandOfExpr{multiply, _2, false},
-    //                                OperandOfExpr{multiply, b, false}};
-    // QTest::newRow("4. Multipliers without expressions in parentheses") << postfixToTree("1 a * 2 b * *", errors) << list_4;
+    // 4. Множители без выражений в скобках
+    QList<OperandOfExpr> list_4 = {OperandOfExpr{nullptr, _1, true},
+                                   OperandOfExpr{multiply, a, false},
+                                   OperandOfExpr{multiply, _2, false},
+                                   OperandOfExpr{multiply, b, false}};
+    QTest::newRow("4. Multipliers without expressions in parentheses") << postfixToTree("1 a * 2 b * *", errors) << list_4;
 
-    // // 5. Множители с выражением в скобках
-    // QList<OperandOfExpr> list_5 = {OperandOfExpr{nullptr, _1, true},
-    //                                OperandOfExpr{division, a, false},
-    //                                OperandOfExpr{division, new NodeOfExprTree("/", _2, b), false}};
-    // QTest::newRow("5. Multipliers expressed in parentheses") << postfixToTree("1 a / 2 b / /", errors) << list_5;
+    // 5. Множители с выражением в скобках
+    QList<OperandOfExpr> list_5 = {OperandOfExpr{nullptr, _1, true},
+                                   OperandOfExpr{division, a, false},
+                                   OperandOfExpr{division, new NodeOfExprTree("/", _2, b), false}};
+    QTest::newRow("5. Multipliers expressed in parentheses") << postfixToTree("1 a / 2 b / /", errors) << list_5;
 
-    // // 6. Унарный минус
-    // QList<OperandOfExpr> list_6 = {OperandOfExpr{new NodeOfExprTree("~"), _1, false}};
-    // QTest::newRow("6. Unary minus") << postfixToTree("1 ~", errors) << list_6;
+    // 6. Унарный минус
+    QList<OperandOfExpr> list_6 = {OperandOfExpr{new NodeOfExprTree("~"), _1, false}};
+    QTest::newRow("6. Unary minus") << postfixToTree("1 ~", errors) << list_6;
 
-    // // 7. Комплексный текст
-    // QList<OperandOfExpr> list_7 = {OperandOfExpr{nullptr, new NodeOfExprTree("~", nullptr, a), true},
-    //                                 OperandOfExpr{plus, b, false},
-    //                                 OperandOfExpr{minus, new NodeOfExprTree("c"), false},
-    //                                 OperandOfExpr{plus, new NodeOfExprTree("d"), false},
-    //                                 OperandOfExpr{minus, new NodeOfExprTree("e"), false},
-    //                                 OperandOfExpr{plus, postfixToTree("1 2 + 3 *", errors), false}};
-    // QTest::newRow("7. Complex text") << postfixToTree("a ~ b + c d - - 1 2 + 3 * +", errors) << list_7 << false;
+    // 7. Комплексный текст
+    QList<OperandOfExpr> list_7 = {OperandOfExpr{nullptr, new NodeOfExprTree("~", nullptr, a), true},
+                                    OperandOfExpr{plus, b, false},
+                                    OperandOfExpr{minus, new NodeOfExprTree("c"), false},
+                                    OperandOfExpr{plus, new NodeOfExprTree("d"), false},
+                                    OperandOfExpr{minus, new NodeOfExprTree("e"), false},
+                                    OperandOfExpr{plus, postfixToTree("1 2 + 3 *", errors), false}};
+    QTest::newRow("7. Complex text") << postfixToTree("a ~ b + c d - - 1 2 + 3 * +", errors) << list_7;
 }
