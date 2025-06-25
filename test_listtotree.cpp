162e894e7b6fec2647ef0a54b38a6837fb35a868
elizavetor <gusevaliza210@yandex.ru>
2025-06-25 11:28:13 +0300

    Реализован слот testListToTree
    Класс Test_listToTree

--- test_listtotree.cpp
+++ test_listtotree.cpp
@@ -3,3 +3,23 @@
 Test_listToTree::Test_listToTree(QObject *parent)
     : QObject{parent}
 {}
+
+void Test_listToTree::testListToTree()
+{
+    QFETCH(QList<OperandOfExpr>, list);
+    QFETCH(NodeOfExprTree*, exp_tree);
+
+    // Установыть связи внутри каждого элемента списка
+    for(int i=0; i < list.size(); i++)
+    {
+        if (list[i].parent) list[i].parent->setRightOperand(list[i].operand);
+    }
+
+    // Установить связи между элементами списка
+    NodeOfExprTree* real_tree = exp_tree->listToTree(list);
+
+    // Сравнить полученное и ожидаемое деревья
+    QStringList path;
+    QString error;
+    QVERIFY2(compareTrees(exp_tree, real_tree, path, error), error.toUtf8());
+}
