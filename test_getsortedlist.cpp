c49e60ee740649c12fcaf047ff8bdc50b3d0c504
elizavetor <gusevaliza210@yandex.ru>
2025-06-25 15:43:04 +0300

    Создание слота Test_getSortedList::testGetSortedList

--- test_getsortedlist.cpp
+++ test_getsortedlist.cpp
@@ -3,3 +3,17 @@
 Test_getSortedList::Test_getSortedList(QObject *parent)
     : QObject{parent}
 {}
+
+void Test_getSortedList::testGetSortedList()
+{
+    QFETCH(NodeOfExprTree*, root);
+    QFETCH(QList<OperandOfExpr>, exp_list);
+
+    // Получить результат
+    QList<OperandOfExpr> real_list = root->getSortedList();
+
+    // Сравнить полученный и ожидаемый списки
+    QString error = compareListsOfNodes(exp_list, real_list);
+    QVERIFY2(error.isEmpty(), qPrintable(error));
+}
+
