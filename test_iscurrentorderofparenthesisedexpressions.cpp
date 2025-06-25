d9d19939313ba0e1d378b8889d682cf4221a9775
elizavetor <gusevaliza210@yandex.ru>
2025-06-25 18:11:01 +0300

    Создание Test_isCurrentOrderOfParenthesisedExpressions
    Для тестирования метода OperandOfExpr::isCurrentOrderOfParenthesisedExpressions

--- test_iscurrentorderofparenthesisedexpressions.cpp
+++ test_iscurrentorderofparenthesisedexpressions.cpp
@@ -4,3 +4,17 @@ Test_isCurrentOrderOfParenthesisedExpressions::Test_isCurrentOrderOfParenthesise
     QObject *parent)
     : QObject{parent}
 {}
+
+void Test_isCurrentOrderOfParenthesisedExpressions::testIsCurrentOrderOfParenthesisedExpressions()
+{
+    QFETCH(OperandOfExpr, elem_1);
+    QFETCH(OperandOfExpr, elem_2);
+    QFETCH(bool, exp_result);
+
+    // Получить результат сравнения
+    bool real_result = elem_1.isCurrentOrderOfMultipliers(elem_2);
+
+    // Сравнить полученный результат с ожидаемым
+    QCOMPARE(real_result, exp_result);
+}
+
