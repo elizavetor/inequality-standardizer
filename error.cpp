f7cadfefce7853645d2aa57c1ea9d9cc18cf6c7a
elizavetor <gusevaliza210@yandex.ru>
2025-06-27 14:46:50 +0300

    Исправление ошибок в конструкторе Error
    Устранена логическая ошибка: присваивался  не элемент списка, а весь список

--- error.cpp
+++ error.cpp
@@ -20,7 +20,7 @@ Error::Error(ErrorType _type, int _pos, QStringList _error_strings)
             error_strings.append("==");
         else if(_error_strings[i] == "~")
             error_strings.append("-");
-        else error_strings = _error_strings;
+        else error_strings[i] = _error_strings[i];
     }
 }
 
